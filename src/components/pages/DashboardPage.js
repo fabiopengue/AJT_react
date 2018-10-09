c08cabb0d54ecabc2f5fd92bde7ad9cb3ba7aa1e
Fabio <fabio.pengue@gmail.com>
2018-10-09 12:42:39 +0200

    Login/Logout

--- src/components/pages/DashboardPage.js
+++ src/components/pages/DashboardPage.js
@@ -1,14 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { logout } from '../../actions/auth';
 
 class DashboardPage extends React.Component {
 
-  render() {
-    return (
-      <div>
-        <h1> Dashboard Page </h1>
-      </div>
-    );
+    logoutHandler = (e) => {
+      logout();
+      this.props.history.push('/')
+    };
+
+    render() {
+      const userAuthenticated = this.props.isAuthenticated;
+      return (
+        <div>
+          <h1> Dashboard Page </h1>
+          {userAuthenticated && <button onClick={e=>this.logoutHandler(e)}> Logout </button> }
+        </div>
+      );
+    }
   }
+
+DashboardPage.propTypes = {
+  isAuthenticated: PropTypes.bool.isRequired,
+  logout: PropTypes.func.isRequired
+};
+
+
+function mapStateToProps(state){
+    return {
+      isAuthenticated: !!state.user.token
+    }
 }
 
-export default DashboardPage;
+export default connect(mapStateToProps, {logout})(DashboardPage);
--- src/index.js
+++ src/index.js
@@ -10,11 +10,17 @@ import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
 import rootReducer from './rootReducer';
 import {composeWithDevTools} from 'redux-devtools-extension';
+import {userLoggedIn} from './actions/auth';
 
 const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(thunk))
 );
+
+if (localStorage.AJTJWT) {
+  const user = {token: localStorage.AJTJWT};
+  store.dispatch(userLoggedIn(user));
+}
 
 ReactDOM.render(<BrowserRouter>
   <Provider store={store}><App /></Provider>
